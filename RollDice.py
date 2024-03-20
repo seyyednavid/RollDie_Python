dbc53cd9de32c4096b63454d2cdb21f0d31629c6
Navid Hejazi <navid.hejazi@Tensator.com>
2024-03-20 12:04:51 +0000

    Dispaly frequency and percentage above each patch (bar)

--- RollDice.py
+++ RollDice.py
@@ -22,6 +22,14 @@ axes.set(xlabel='Die Value', ylabel='Frequency')  # label the axes
 # scale y-axis by 10% to make room for text above bars
 axes.set_ylim(top=max(frequencies) * 1.10)
 
+# display frequency & percentage above each patch (bar)
+for bar, frequency in zip(axes.patches, frequencies):
+    text_x = bar.get_x() + bar.get_width() / 2.0  
+    text_y = bar.get_height() 
+    text = f'{frequency:,}\n{frequency / len(rolls):.3%}'
+    axes.text(text_x, text_y, text, 
+              fontsize=11, ha='center', va='bottom')
+
 plt.show()  # display graph 
 
 
@@ -60,11 +68,5 @@ plt.show()  # display graph
 
 
 
-# # display frequency & percentage above each patch (bar)
-# for bar, frequency in zip(axes.patches, frequencies):
-#     text_x = bar.get_x() + bar.get_width() / 2.0  
-#     text_y = bar.get_height() 
-#     text = f'{frequency:,}\n{frequency / len(rolls):.3%}'
-#     axes.text(text_x, text_y, text, 
-#               fontsize=11, ha='center', va='bottom')
+
 
